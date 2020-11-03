2de21d20e849b9a745136f6960fefdc84f195cc0
moljac <mcvjetko@holisticware.net>
2020-11-03 08:37:35 +0100

    multi collection (List) sample

--- samples/projects/console/Sample.20201102/Program.cs
+++ samples/projects/console/Sample.20201102/Program.cs
@@ -19,11 +19,24 @@ namespace Sample.Original
                 1.1, 2.2, 4.4, 8.9, 32.2, 56.0, 34.2
             };
 
+            List<List<int>> ints_multi = new List<List<int>>()
+            {
+                new List<int>
+                {
+                    1, 2, 4, 8, 32, 56, 34
+                },
+                new List<int>
+                {
+                    11, 22, 44, 88, 55, 99, 342
+                },
+            };
+
             string json = null;
             
             json = ints.Visualize();
             json = doubles.Visualize();
-            
+            json = ints_multi.Visualize();
+
             return;
         }
     }
